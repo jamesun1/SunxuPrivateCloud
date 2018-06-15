aa3e5bd1bfccd483c3bd22f9b488e0d72b3c3f38
sunxu2 <1017294721@qq.com>
2018-06-15 18:17:41 +0800

    增加redis

--- src/main/java/com/physical/services/imp/UserServiceImp.java
+++ src/main/java/com/physical/services/imp/UserServiceImp.java
@@ -1,12 +1,16 @@
 package com.physical.services.imp;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.physical.RedisService;
 import com.physical.mapper.UserinfoMapper;
 import com.physical.model.Userinfo;
 import com.physical.services.UserService;
 import com.physical.util.ApiResult;
+import com.physical.util.JedisUtils;
 import com.physical.util.LogicalException;
 
 @Service
@@ -14,12 +18,16 @@ public class UserServiceImp implements UserService{
 	
 	@Autowired
 	private UserinfoMapper userinfoMapper;
+	@Autowired
+	private RedisService redisService;
 
 	@Override
 	public ApiResult login(Userinfo user) throws LogicalException {
 		try {
 			Userinfo userinfo = userinfoMapper.selectOneByPerson();
 			if(userinfo != null) {
+				redisService.set(userinfo.getUserid(), UUID.randomUUID().toString());
+				System.out.println(redisService.get(userinfo.getUserid()));
 				return ApiResult.success(userinfo);
 			}else {
 				return ApiResult.fail("登录失败");
