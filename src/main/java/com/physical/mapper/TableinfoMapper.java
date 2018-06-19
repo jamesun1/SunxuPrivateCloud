ab8146b87c8ab764c633d492ce4cae24d73bc9de
sunxu2 <1017294721@qq.com>
2018-06-19 17:17:39 +0800

    11

--- src/main/java/com/physical/mapper/TableinfoMapper.java
+++ src/main/java/com/physical/mapper/TableinfoMapper.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.physical.model.Tableinfo;
 import com.physical.util.MyMapper;
 
-public interface TableinfoMapper extends MyMapper<Tableinfo>{
+public interface TableinfoMapper {
 
 	List<Tableinfo> selectAllByRole(Tableinfo table);
 }
