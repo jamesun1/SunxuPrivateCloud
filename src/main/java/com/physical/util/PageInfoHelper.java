7705629061af1e7e3dea9925cc267e20779687d6
sunxu2 <1017294721@qq.com>
2018-06-20 14:51:49 +0800

    11

---
+++ src/main/java/com/physical/util/PageInfoHelper.java
@@ -0,0 +1,16 @@
+package com.physical.util;
+
+import org.springframework.beans.BeanUtils;
+
+import com.physical.model.PageInfo;
+
+public class PageInfoHelper {
+
+	public static PageInfo pageInit(Object o) {
+		PageInfo pageInfo = new PageInfo();
+		BeanUtils.copyProperties(o, pageInfo);
+		
+		pageInfo.setStartNums(pageInfo.getPageSize()*(pageInfo.getCurrentPage()-1));
+		return pageInfo;
+	}
+}
