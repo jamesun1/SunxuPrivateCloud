6b51cd47bf3d0e5516cab64aa3a4fda3bf8b9c20
sunxu <1017294721@qq.com>
2018-07-09 22:58:42 +0800

    111

--- src/main/java/com/physical/services/imp/TreeServiceImp.java
+++ src/main/java/com/physical/services/imp/TreeServiceImp.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.Estate.services.impl.CustomerServiceImpl;
 import com.physical.mapper.TreeinfoMapper;
 import com.physical.model.Treeinfo;
 import com.physical.services.TreeService;
