e2e6f4d84d3ba756f245789d1ba7bb638d12b1e8
sunxu <1017294721@qq.com>
2018-07-09 22:57:45 +0800

    111

--- src/main/java/com/physical/services/imp/TreeServiceImp.java
+++ src/main/java/com/physical/services/imp/TreeServiceImp.java
@@ -3,9 +3,12 @@ package com.physical.services.imp;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Estate.services.impl.CustomerServiceImpl;
 import com.physical.mapper.TreeinfoMapper;
 import com.physical.model.Treeinfo;
 import com.physical.services.TreeService;
@@ -14,6 +17,7 @@ import com.physical.util.LogicalException;
 
 @Service
 public class TreeServiceImp implements TreeService{
+	private static Logger log = LoggerFactory.getLogger(TreeServiceImp.class);
 
 	@Autowired
 	private TreeinfoMapper treeinfoMapper;
@@ -47,7 +51,7 @@ public class TreeServiceImp implements TreeService{
 			List<Treeinfo> treeinfoList = treeinfoMapper.selectInfoByParentid(tree);
 			return ApiResult.success(treeinfoList);	
 		}catch (Exception e) {
-			
+			log.warn(e.toString());
 			throw new LogicalException("操作失败");
 		}
 	}
