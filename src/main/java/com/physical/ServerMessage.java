a0ea7df3299b1e6d06dc17eee4229763d5d57b1e
sunxu2 <1017294721@qq.com>
2018-07-10 14:08:50 +0800

    新增websocket

---
+++ src/main/java/com/physical/ServerMessage.java
@@ -0,0 +1,16 @@
+package com.physical;
+public class ServerMessage {
+    private String responseMessage;
+
+    public ServerMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
---
+++ src/main/java/com/physical/controller/WebSocketAction.java
@@ -0,0 +1,31 @@
+package com.physical.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+
+import com.physical.ClientMessage;
+import com.physical.ServerMessage;
+
+@Controller
+public class WebSocketAction {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @MessageMapping("/sendTest")
+    @SendTo("/topic/subscribeTest")
+    public ServerMessage sendDemo(ClientMessage message) {
+        logger.info("接收到了信息" + message.getName());
+        return new ServerMessage(message.getName());
+    }
+
+    @SubscribeMapping("/subscribeTest")
+    public ServerMessage sub() {
+        logger.info("XXX用户订阅了我。。。");
+        return new ServerMessage("感谢你订阅了我。。。");
+    }
+
+}
