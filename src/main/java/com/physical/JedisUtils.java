256b27ae3f7efb00132456796404d07520bf0e25
sunxu2 <1017294721@qq.com>
2018-07-17 17:14:37 +0800

    111

--- src/main/java/com/physical/JedisUtils.java
+++ src/main/java/com/physical/JedisUtils.java
@@ -1,7 +1,12 @@
 package com.physical;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+
 import com.alibaba.fastjson.JSONObject;
-import com.physical.model.Userinfo;
 import com.physical.util.SerializeUtil;
 import com.physical.vo.RedisTest;
 
@@ -12,107 +17,149 @@ import redis.clients.jedis.JedisPoolConfig;
 public class JedisUtils {
 
 	private static JedisPool pool = null;
-	
-	
+	static InputStream inStream = JedisUtils.class.getClassLoader().getResourceAsStream("redis.properties");
+
 	/**
 	 * 打开Jedis连接
+	 * 
+	 * @throws IOException
 	 */
-	public static Jedis openJedis() {
+	public static Jedis openJedis() throws IOException {
+		Properties prop = new Properties();
+		prop.load(inStream);
+		String host = prop.getProperty("redis.host");
+		String password = prop.getProperty("redis.password");
+		String port = prop.getProperty("redis.port");
+		String timeout = prop.getProperty("redis.timeout");
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
 		// 设置最大10个连接
 		jedisPoolConfig.setMaxTotal(10);
-		pool = new JedisPool(jedisPoolConfig, "119.29.108.164",6379,2000,"sunxu");
+		pool = new JedisPool(jedisPoolConfig, host, Integer.valueOf(port), Integer.valueOf(timeout), password);
 		Jedis jedis = pool.getResource();
 		return jedis;
 	}
-	
+
 	/**
 	 * 关闭连接
+	 * 
 	 * @param jedis
 	 */
 	public static void closeJedis(Jedis jedis) {
 		jedis.close();
 		pool.close();
 	}
-	
+
 	/**
 	 * 传入对象最为键值对
+	 * 
 	 * @param key
 	 * @param value
 	 */
-	public static void append(byte[] key,Object value) {
-		Jedis jedis = openJedis();
-		byte[] b = SerializeUtil.serialize(value);
-		jedis.append(key, b);
-		closeJedis(jedis);
+	public static void append(byte[] key, Object value) {
+		try {
+			Jedis jedis = openJedis();
+			byte[] b = SerializeUtil.serialize(value);
+			jedis.append(key, b);
+			closeJedis(jedis);
+		} catch (Exception e) {
+
+		}
+
 	}
-	
+
 	/**
 	 * 传入字符串作为键值对
+	 * 
 	 * @param key
 	 * @param value
 	 */
-	public static void append(String key,String value) {
-		Jedis jedis = openJedis();
-		jedis.append(key, value);
-		closeJedis(jedis);
+	public static void append(String key, String value) {
+		try {
+			Jedis jedis = openJedis();
+			jedis.append(key, value);
+			closeJedis(jedis);
+		} catch (Exception e) {
+		}
+
 	}
-	
+
 	/**
 	 * 根据字符串key获取值
+	 * 
 	 * @param key
 	 * @return
 	 */
 	public static String getValue(String key) {
-		Jedis jedis = openJedis();
-		String value = jedis.get(key);
-		return value;
+		try {
+			Jedis jedis = openJedis();
+			String value = jedis.get(key);
+			return value;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
 	}
-	
+
 	/**
 	 * 根据对象key查找
+	 * 
 	 * @param key
 	 * @return
 	 */
 	public static Object getValue(byte[] key) {
-		Jedis jedis = openJedis();
-		byte[] b = jedis.get(key);
-		Object o = SerializeUtil.unserialize(b);
-		return o;
+		try {
+			Jedis jedis = openJedis();
+			byte[] b = jedis.get(key);
+			Object o = SerializeUtil.unserialize(b);
+			return o;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
 	}
-	
+
 	/**
 	 * 删除
+	 * 
 	 * @param key
 	 */
 	public static void remove(String key) {
-		Jedis jedis = openJedis();
-		jedis.del(key);
+		try {
+			Jedis jedis = openJedis();
+			jedis.del(key);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
-	
+
 	/**
 	 * 删除
+	 * 
 	 * @param key
 	 */
 	public static void remove(byte[] key) {
-		Jedis jedis = openJedis();
-		jedis.del(key);
+		try {
+			Jedis jedis = openJedis();
+			jedis.del(key);
+		} catch (Exception e) {
+		}
 	}
-	
-	public static void main(String[] args) {
+
+	@Test
+	public void test() {
 		RedisTest test = new RedisTest();
 		test.setName("小明");
 		test.setAge("22");
 		test.setHeight("177");
 		test.setWeight("74.7");
-		
+
 		append("sunxu".getBytes(), test);
 		RedisTest aa = new RedisTest();
 		aa = (RedisTest) getValue("sunxu".getBytes());
-		
+
 		JSONObject json = (JSONObject) JSONObject.toJSON(aa);
 		System.out.println(json.toJSONString());
-		
+
 		remove("sunxu".getBytes());
 	}
 }
